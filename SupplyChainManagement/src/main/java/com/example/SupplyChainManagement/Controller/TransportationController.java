d7c19c1fda11e663b6ddf4a5e58333b391f4e7cc
MohanReddy <mohanreddyderedla148@gmail.com>
2024-11-25 10:49:09 +0530

    Controller Layer creation Done

---
+++ SupplyChainManagement/src/main/java/com/example/SupplyChainManagement/Controller/TransportationController.java
@@ -0,0 +1,8 @@
+package com.example.SupplyChainManagement.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransportationController {
+
+}
