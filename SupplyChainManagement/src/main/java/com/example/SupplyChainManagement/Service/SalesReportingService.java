36e88043e9bf48871b4149f6a9291fd5d88d99a1
MohanReddy <mohanreddyderedla148@gmail.com>
2024-11-25 13:21:09 +0530

    SalesReport Done

--- SupplyChainManagement/src/main/java/com/example/SupplyChainManagement/Service/SalesReportingService.java
+++ SupplyChainManagement/src/main/java/com/example/SupplyChainManagement/Service/SalesReportingService.java
@@ -1,8 +1,25 @@
 package com.example.SupplyChainManagement.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.SupplyChainManagement.Models.SalesReport;
+import com.example.SupplyChainManagement.Repository.OrdersRepository;
 
 @Service
 public class SalesReportingService {
+	@Autowired
+    private OrdersRepository ordersRepository;
+
+	public SalesReport generateAnnualReport(String year) {
+		int totalOrders = ordersRepository.findAll().size();
+        double totalSales = ordersRepository.findAll().stream().mapToDouble(o -> o.getQuantity() * 100).sum();
+
+        SalesReport report = new SalesReport();
+        report.setYear(year);
+        report.setTotalOrders(totalOrders);
+        report.setTotalSales(totalSales);
+        return report;
+	}
 
 }
