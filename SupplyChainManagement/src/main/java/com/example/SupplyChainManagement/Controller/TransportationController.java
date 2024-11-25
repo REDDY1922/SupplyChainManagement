3fbacb1d693abff1b57870e193c0533e0c7a4e54
MohanReddy <mohanreddyderedla148@gmail.com>
2024-11-25 12:58:18 +0530

    Transportation service Done

--- SupplyChainManagement/src/main/java/com/example/SupplyChainManagement/Controller/TransportationController.java
+++ SupplyChainManagement/src/main/java/com/example/SupplyChainManagement/Controller/TransportationController.java
@@ -1,8 +1,23 @@
 package com.example.SupplyChainManagement.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class TransportationController {
+import com.example.SupplyChainManagement.Models.Shipment;
+import com.example.SupplyChainManagement.Service.TransportationService;
 
+@RestController
+@RequestMapping("/transport")
+public class TransportationController {
+	
+	 @Autowired
+	    private TransportationService transportationService;
+
+	    @PostMapping("/assignShipment")
+	    public Shipment assignShipment(@RequestParam String orderId, @RequestParam double weight, @RequestParam double distance) {
+	        return transportationService.assignShipment(orderId, weight, distance);
+	    }
 }
--- SupplyChainManagement/src/main/java/com/example/SupplyChainManagement/Service/TransportationService.java
+++ SupplyChainManagement/src/main/java/com/example/SupplyChainManagement/Service/TransportationService.java
@@ -1,8 +1,32 @@
 package com.example.SupplyChainManagement.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.SupplyChainManagement.Models.Shipment;
+import com.example.SupplyChainManagement.Repository.ShipmentRepository;
 
 @Service
 public class TransportationService {
+	@Autowired
+    private ShipmentRepository shipmentRepository;
 
+	public Shipment assignShipment(String orderId, double weight, double distance) {
+		Shipment shipment = new Shipment();
+        shipment.setOrderId(orderId);
+        shipment.setWeight(weight);
+        shipment.setDistance(distance);
+        shipment.setFreightCost(calculateFreightCost(weight, distance));
+        shipment.setRoute(optimizeRoute(distance));
+        shipment.setStatus("In Transit");
+        return shipmentRepository.save(shipment);
+	}
+	
+	private double calculateFreightCost(double weight, double distance) {
+        return (weight * 5) + (distance * 0.1);
+    }
+
+    private String optimizeRoute(double distance) {
+        return distance > 500 ? "Highway" : "City Roads";
+    }
 }
